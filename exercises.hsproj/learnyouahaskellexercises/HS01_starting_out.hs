c999be2504d76251a87f0bd516ce78958e1aa34a
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-22 08:46:14 +0100

    Add find index, isPalindrome and duplicate

--- exercises.hsproj/learnyouahaskellexercises/HS01_starting_out.hs
+++ exercises.hsproj/learnyouahaskellexercises/HS01_starting_out.hs
@@ -1,23 +1,19 @@
-{-
- -Once you've installed Haskell from http://www.haskell.org/platform/, load the interpreter with the command ghci.
- -
- -You can load (and reload) this file in the interpreter with the command: ":l 2-starting-out.hs"
- -
- -The first function has been completed as an example. All the other functions are undefined.
- -They can be implemented in one line using the material covered in http://learnyouahaskell.com/starting-out
- -
- -All indices are zero based.
- -}
+module HS01_starting_out where
 
 -- Find the penultimate element in list l
 penultimate l = last (init l)
 
+
 -- Find the element at index k in list l
--- For example: "findK 2 [0,0,1,0,0,0]" returns 1
-findK k l = undefined
+-- Example: "findK 2 [0,0,1,0,0,0]" returns 1
+findK k l = l !! k
+
 
 -- Determine if list l is a palindrome
-isPalindrome l = undefined
+-- Example: "isPalindrome noon" returns true
+isPalindrome l = l == reverse l
+
+
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
@@ -25,14 +21,16 @@ isPalindrome l = undefined
  - (You can see the function definition by typing ":t concat" into the interpreter. Perhaps try this with other variables and functions)
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
- -}
-duplicate xs = undefined
+-}
+duplicate xs = concat [[x, x] | x <- xs]
+
+
 
 {-
  - Imitate the functinality of zip
  - The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
- -}
+-}
 ziplike xs ys = undefined
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
